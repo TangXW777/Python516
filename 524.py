dc6dc05d0bb3688852b4389298c5f4775adbafa2
TangXW777 <970097457@qq.com>
2017-05-24 13:49:18 +0800

    524 test 模块

---
+++ 524.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+# -*- encoding: utf-8 -*-
+'a test module'
+__author__ = 'TangXW'
+
+#========= 使用模块 ===========
+#import sys
+#def test():
+#	args = sys.argv;
+#	if len(args) == 1:
+#		print('hello world');
+#	elif len(args) == 2:
+#		print('hello world! %s' % args[1]);
+#	else:
+#		print('too many arguments');
+#	
+#if __name__ == '__main__':
+#	test();
+#		
+#print(__doc__)
+
+
+#=========== 引入Pillow ==============
+#from PIL import Image
+#im = Image.open('test.open');
+#print(im.format, im.size, im.mode);
+#PNG(400, 300)RGB
+#im.thumbnail((200, 100));
+#im.save('thumb.jpg','JPEG');
